37de49ebb3225ece5418b29fcfcb4aa55b266950
Peter Kyeyune <peterkayk@gmail.com>
2024-04-04 11:49:09 +0300

    Delete App.java and update App.class

--- medicare_capstone/src/main/java/com/Main.java
+++ medicare_capstone/src/main/java/com/Main.java
@@ -2,6 +2,16 @@ package com;
 
 import java.util.Scanner;
 
+
+package com.ihfms;
+
+import java.util.Scanner;
+
+import com.observer.ConsoleMessageNotifier;
+import com.observer.MessageNotifier;
+import com.observer.MessageService;
+import com.observer.MessagingSystem;
+
 import com.BillingCollection.Interact_Billing.Billing;
 import com.Decorator.AppointmentEntry;
 
@@ -75,5 +85,27 @@ public class App {
         } else {
             System.out.println("Invalid facility type");
         }
+    }
+}
+
+package com.ihfms;
+
+import java.util.Scanner;
+
+import com.observer.ConsoleMessageNotifier;
+import com.observer.MessageNotifier;
+import com.observer.MessageService;
+import com.observer.MessagingSystem;
+
+public class Main {
+    public static void main(String[] args) {
+        MessageService messageService = new MessageService();
+        MessageNotifier notifier = new ConsoleMessageNotifier();
+        Scanner scanner = new Scanner(System.in);
+
+        MessagingSystem messagingSystem = new MessagingSystem(messageService, notifier, scanner);
+        messagingSystem.start();
+
+        scanner.close();
     }
 }
