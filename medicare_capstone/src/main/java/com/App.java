ba458645f6e9bea3d308396226a1c89a22a4570a
MUTEGEKIDAVID <93861880+MUTEGEKIDAVID@users.noreply.github.com>
2024-04-11 15:00:37 +0300

    ""

--- medicare_capstone/src/main/java/com/App.java
+++ medicare_capstone/src/main/java/com/App.java
@@ -5,16 +5,8 @@ import java.util.List;
 import java.util.Scanner;
 
 import com.Authentication.*;
-import com.BillingCollection.Interact_Billing.Billing;
-import com.Decorator.AppointmentEntry;
 
-import com.Models.Invoice;
-import com.Models.Patient;
-import com.facilities.Facility;
-import com.facilities.FacilityFactory;
-
-import com.healthworkers.HealthWorker;
-import com.healthworkers.HealthWorkerFactory;
+import com.chainofResponsibility.*;
 
 public class App {
     @SuppressWarnings({ "resource", "unused" })
@@ -54,63 +46,17 @@ public class App {
 
         if (isAuthenticated) {
             System.out.println("Authentication successful!! Welcome " + username);
+            
 
-            System.out.println("SELECT FACILITY OF INTEREST ");
-            System.out.println("1.Hospital ");
-            System.out.println("2.Clinic");
-            System.out.println("3.Pharmacy");
+            IRole rolechain1 = new PatientRole();
+            IRole rolechain2 = new HealthworkerRole();
 
-            // selecting a preferred type of facility
-            System.out.println("Enter facility type:.. ");
-            Integer facilityType = scanner.nextInt();
+            rolechain1.setnextRole(rolechain2);
+            rolechain1.execute(role);
 
-            Facility facility = FacilityFactory.createFacility(facilityType, "Mulago", "Kampala");
-            if (facility != null) {
-                facility.printDetails();
-                // select role in this facility either as patient or healthy worker
-                System.out.println("Select role: ");
-                Integer roleType = scanner.nextInt();
 
-                if (roleType == 1) {
-                    Patient patient = new Patient(null, null, null, 23);
-                    HERE: patient.displayPatientRoles();
-                    Integer patientTask = scanner.nextInt();
-                    while (true) {
 
-                        if (patientTask == 1) {
-                            // make appointment
-                            AppointmentEntry appointmentEntry = new AppointmentEntry();
-                            appointmentEntry.displayAppointment();
-
-                            System.out.println("Do you want to go back to the main menu? (yes/no)");
-                            String response = scanner.next();
-                            if (response.equalsIgnoreCase("yes")) {
-                                break; // Breaks the loop and goes back to the main menu
-                            }
-
-                        } else if (patientTask == 2) {
-                            Invoice invoice = new Invoice();
-                            Billing billing = new Billing(patient, invoice);
-                            billing.processBills();
-
-                        }
-                    }
-
-                } else if (roleType == 2) {
-                    // if healthyworker select typr of healthyworker
-                    System.out.println("..............WHAT KIND OF HEALTH WORKER ARE YOU ");
-                    System.out.println("1.Doctor ");
-                    System.out.println("2.Nurse");
-                    Integer healthWorkerType = scanner.nextInt();
-
-                    HealthWorker healthyWorker = HealthWorkerFactory.getHealthyWorker(healthWorkerType);
-                    healthyWorker.templateMethod();
-
-                }
-
-            } else {
-                System.out.println("Invalid facility type");
-            }
+           
 
             new MessagingMain();
             scanner.close();
